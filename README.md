420ed4f2cebaa107e608cac0515527b29d9fbd4f
Francois Massonnet <francois.massonnet@uclouvain.be>
2019-08-11 23:27:37 +0200

    New files, map

--- README.md
+++ README.md
@@ -1,6 +1,42 @@
-- List of climbs: https://fr.m.wikipedia.org/wiki/Hors_cat%C3%A9gorie
+- List of climbs: https://fr.m.wikipedia.org/wiki/Hors_cat%C3%A9gorie#Tour_de_France
 
 How to add a new climb?
   1. Create a strava itinerary
   2. Export as GPX
   3. Place in ./data/ 
+
+  
+ 
+The routes are the ones of the last climb from the Tour de France.
+
+
+Col Agnel		https://fr.m.wikipedia.org/wiki/Tour_de_France_2011
+
+L'Alpe d'Huez	
+	https://fr.m.wikipedia.org/wiki/12e_%C3%A9tape_du_Tour_de_France_2018
+	https://www.climbbybike.com/climb.asp?Col=Alpe-dHuez&qryMountainID=5
+Andorre Arcalis
+	https://www.climbbybike.com/climb.asp?Col=Arcalis&qryMountainID=3926 (arrivée trop haut)
+	https://www.cols-cyclisme.com/pyrenees-centrales/andorre/station-d-arcalis-depuis-ordino-c1155.htm
+Col d'Aubisque
+	https://fr.m.wikipedia.org/wiki/19e_%C3%A9tape_du_Tour_de_France_2018
+	! Bug Strava ! 
+Port de Balès
+	https://fr.wikipedia.org/wiki/12e_%C3%A9tape_du_Tour_de_France_20177
+	https://www.climbbybike.com/climb.asp?Col=Port-de-Bales&qryMountainID=7057
+Plateau de Beille
+	https://fr.m.wikipedia.org/wiki/12e_%C3%A9tape_du_Tour_de_France_2015
+	https://www.cols-cyclisme.com/pyrenees-centrales/france/plateau-de-beille-depuis-les-cabannes-c27.htm
+Col de la Biche
+	https://fr.m.wikipedia.org/wiki/9e_%C3%A9tape_du_Tour_de_France_2017
+	https://www.climbbybike.com/climb.asp?Col=Col-de-la-Biche&qryMountainID=6059
+Montée de Bisanne
+	https://fr.m.wikipedia.org/wiki/11e_%C3%A9tape_du_Tour_de_France_2018
+	https://www.cols-cyclisme.com/beaufortain/france/signal-de-bisanne-depuis-villard-sur-doron-c305.htm
+	https://www.climbbybike.com/climb.asp?Col=Signal-de-Bisanne&qryMountainID=7134
+Cime de la Bonette
+	https://fr.m.wikipedia.org/wiki/16e_%C3%A9tape_du_Tour_de_France_2008
+	
+Chamrousse
+	https://fr.m.wikipedia.org/wiki/13e_%C3%A9tape_du_Tour_de_France_2014
+	https://www.cols-cyclisme.com/belledonne/france/roche-beranger-station-de-chamrousse-depuis-uriage-les-bains-c130.htm
--- makefig.py
+++ makefig.py
@@ -10,6 +10,7 @@ Created on Wed Aug  7 22:38:31 2019
 import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 from   mpl_toolkits.basemap import Basemap
 from   scipy import interpolate
 from   mpl_toolkits.mplot3d import Axes3D
@@ -18,10 +19,22 @@ from   mpl_toolkits.mplot3d import Axes3D
 
 # Input files
 filein = [
-          ["Col Agnel (depuis Casteldelfino).gpx",         148.15],
-          ["L'Alpe d'Huez (depuis Le Bourg-d'Oisans).gpx", 124.32],
-          ["Andorre Arcalis (depuis Ordino).gpx"         ,  89.88],
-          ["Col d'Aubisque (depuis Argelès-Gazost).gpx"  ,  97.05],
+          ["Col Agnel (depuis Casteldelfino)"        ],
+          #["", 0], ["", 0], ["", 0], ["", 0], ["", 0], ["", 0], ["", 0], 
+          #["", 0], ["", 0], ["", 0], ["", 0], ["", 0], ["", 0], ["", 0], 
+          #["", 0], ["", 0], ["", 0], ["", 0], ["", 0], ["", 0], ["", 0], 
+          #["", 0], ["", 0], ["", 0], ["", 0], ["", 0], ["", 0], ["", 0], 
+          #["", 0], ["", 0], ["", 0], ["", 0], ["", 0], ["", 0], ["", 0], 
+          #["", 0], ["", 0], ["", 0], ["", 0], ["", 0], ["", 0], 
+          ["L'Alpe d'Huez (depuis Le Bourg-d'Oisans)"],
+          ["Andorre Arcalis (depuis Ordino)"         ],
+          ["Col d'Aubisque (depuis Argelès-Gazost)"  ],
+          ["Port de Balès (depuis Mauléon-Barousse)" ],
+          ["Plateau de Beille (depuis Les Cabannes)" ],
+          ["Col de la Biche (depuis Gignez)"         ],
+          ["Montée de Bisanne (depuis Villard-Sur-Doron)"],
+          ["Cime de la Bonette (depuis Saint-Etienne-de-Tinée)"],
+          ["Chamrousse (depuis Uriage-les-Bains)"]
          ]
 
 # Colors
@@ -47,85 +60,136 @@ def projection(lon, lat):
     
     return (x - x[0]) / 1e3, (y - y[0]) / 1e3
 
-    
+
+def difficulty_index(H, D, T):
+    # From https://www.climbbybike.com/fr/climbbybike_index.asp
+    # H : altitude gain in meters
+    # D : distance riden in meters
+    # T : top altitude
+    out = (H*100/D)*4 + H**2/D + D/1000 
+    if T > 1000.0:
+        out += (T-1000)/100
+    return out
+
+# Projection for final map
+
+m = Basemap(llcrnrlon = -1.7 ,llcrnrlat = 41.3, urcrnrlon = 9.0 ,
+            urcrnrlat = 48.8,
+            projection='lcc',lat_1=43.5, lat_2=45.3, lon_0 = 2.4,
+            resolution ='c', area_thresh=1000.)
+ 
+print('TACHY')
+
 # Loop over files
     
-fig = plt.figure("figall", figsize = (12, 6))
+fig = plt.figure("figall", figsize = (32, 16))
 
 for file in enumerate(filein):
-    f = file[1][0]
+    id = file[0] + 1
     jf = file[0]
-    score = file[1][1]
+    if jf + 1 >= 34:
+        plt.subplot(4, 12, jf + 4)
+    else:
+        plt.subplot(4, 12, jf + 1)
+
+    if not os.path.exists("./data/" + file[1][0] + ".gpx"):
+        print("hello")
+        plt.plot((0, 1), (0, 1))
+        plt.title(file[0] + 1)
+    else:
+        
+        print(file)
+        f = file[1][0] + ".gpx"
+
+        
+        # Initialize coordinates
+        lon = list()
+        lat = list()
+        z   = list()
+
+        # Parse XML file
+        tree = ET.parse("./data/" + f)
+        root = tree.getroot()
+        
+        # Load data
+        for j in range(len(root[1][3])):
+            lon.append(float(root[1][3][j].attrib["lon"]))
+            lat.append(float(root[1][3][j].attrib["lat"]))
+            z.append(float(root[1][3][j][0].text))
+        
+        # Convert (lon,lat) to (x,y)
+        x, y = projection(lon, lat)
+        # Convert elevation to array
+        z = np.array(z)
+        
+        
+        # Smooth
+        #neighb = d[1:] - d[:-1]
+        #num_true_pts = len(z) * 2
+        #num_sample_pts = len(z)
+        #tck, u = interpolate.splprep([x, y, z], s = 0.5, k = 5)
+        #u_fine = np.linspace(0,1,num_true_pts)
+        #x_fine, y_fine, z_fine = interpolate.splev(u_fine, tck)
+        
+        # Compute distance from start
+        d = np.append([0.0], np.cumsum(np.sqrt(np.diff(x     ) ** 2 + 
+                                               np.diff(y     ) ** 2)))
+        #d_fine = np.append([0.0], np.cumsum(np.sqrt(np.diff(x_fine) ** 2 + np.diff(y_fine) ** 2)))
+        #sl_fine = np.diff(z_fine) / np.diff(d_fine * 1000.0) * 100.0
+        
+        # Compute slopes, per dd horizontal steps
+        slope = list()
+        
+        dd = 0.2 # step, km. 200 m is a good tradeoff that avoids overestimating
+                 # slopes when two successive points miss a needle turn, and is 
+                 # short enough to render realistic max values. And the mean is well
+                 # estimated
+        d2 = d[-1]
+        d1 = d2 - dd
+        
+        while d1 > 0:
+          xx = d[(d>d1) * (d<=d2)]
+          zz = z[(d>d1) * (d<=d2)]
+          try:
+              slope.append(np.polyfit(xx * 1000, zz, 1)[0] * 100.0)
+          except TypeError:
+              pass
+          d1 -= dd# / 10.0
+          d2 -= dd# / 10.0
+          
+        slope = np.array(slope)
+        
+        score = difficulty_index((z[-1] - z[0]), (d[-1] - d[0]) * 1000.0, z[-1])
+        
+        # Plots
+        plt.fill_between(d, z, color = colors[jf])
+        plt.ylim(0, 3000)
+        plt.xlim(0, d[-1])
+        plt.text(0, 0, str(np.round(np.mean(slope), 1)) + "% (" + str(np.round(np.max(slope))) + "%)")
+        plt.title(str(id) + ". " + f.split(" (")[0] + "\n" + str(np.round(score)))
+        
+        plt.tight_layout()
     
-    plt.subplot(6, 8, jf + 1)
-    # Initialize coordinates
-    lon = list()
-    lat = list()
-    z   = list()
+        plt.subplot(2, 4, 8)
+        xx, yy = m(lon[-1], lat[-1])
+        plt.scatter(xx, yy, 50, colors[jf])
+        plt.text(xx, yy, str(id), color = "white", ha = "center", va = "center")
+ 
+plt.subplot(2, 4, 8)           
+m.drawcoastlines(linewidth = 1)
+m.drawcountries(linewidth = 1)   
+#
+#m = Basemap(llcrnrlon = -1.7 ,llcrnrlat = 41.3, urcrnrlon = 9.0 ,
+#            urcrnrlat = 48.8,
+#            projection='lcc',lat_1=43.5, lat_2=45.3, lon_0 = 2.4,
+#            resolution ='l', area_thresh=1000.)
+#m.drawcoastlines(linewidth = 1)
+#m.drawcountries(linewidth = 1)
+
     
-    # Parse XML file
-    tree = ET.parse("./data/" + f)
-    root = tree.getroot()
-    
-    # Load data
-    for j in range(len(root[1][3])):
-        lon.append(float(root[1][3][j].attrib["lon"]))
-        lat.append(float(root[1][3][j].attrib["lat"]))
-        z.append(float(root[1][3][j][0].text))
-    
-    # Convert (lon,lat) to (x,y)
-    x, y = projection(lon, lat)
-    # Convert elevation to array
-    z = np.array(z)
-    
-    
-    # Smooth
-    #neighb = d[1:] - d[:-1]
-    #num_true_pts = len(z) * 2
-    #num_sample_pts = len(z)
-    #tck, u = interpolate.splprep([x, y, z], s = 0.5, k = 5)
-    #u_fine = np.linspace(0,1,num_true_pts)
-    #x_fine, y_fine, z_fine = interpolate.splev(u_fine, tck)
-    
-    # Compute distance from start
-    d = np.append([0.0], np.cumsum(np.sqrt(np.diff(x     ) ** 2 + 
-                                           np.diff(y     ) ** 2)))
-    #d_fine = np.append([0.0], np.cumsum(np.sqrt(np.diff(x_fine) ** 2 + np.diff(y_fine) ** 2)))
-    #sl_fine = np.diff(z_fine) / np.diff(d_fine * 1000.0) * 100.0
-    
-    # Compute slopes, per dd horizontal steps
-    slope = list()
-    
-    dd = 0.2 # step, km. 200 m is a good tradeoff that avoids overestimating
-             # slopes when two successive points miss a needle turn, and is 
-             # short enough to render realistic max values. And the mean is well
-             # estimated
-    d2 = d[-1]
-    d1 = d2 - dd
-    
-    while d1 > 0:
-      xx = d[(d>d1) * (d<=d2)]
-      zz = z[(d>d1) * (d<=d2)]
-      try:
-          slope.append(np.polyfit(xx * 1000, zz, 1)[0] * 100.0)
-      except TypeError:
-          pass
-      d1 -= dd# / 10.0
-      d2 -= dd# / 10.0
-      
-    slope = np.array(slope)
-    
-    
-    # Plots
-    plt.fill_between(d, z, color = colors[jf])
-    plt.ylim(0, 3000)
-    plt.xlim(0, d[-1])
-    plt.text(0, 0, str(np.round(np.mean(slope), 1)) + "% (" + str(np.round(np.max(slope))) + "%)")
-    plt.title(f.split(" (")[0] + "\n" + str(score))
-    
-    plt.tight_layout()
-    
+plt.subplots_adjust(hspace = 0.2)
 plt.savefig("./figs/figall.png", dpi = 300)
+
     #plt.savefig("./figs/" + f[:-4] + ".pdf")
     
 #    plt.subplot(2, 2, 1)
